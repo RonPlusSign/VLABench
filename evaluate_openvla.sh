3d7529f2617710a6ed103593046163624fdcbfce
Andrea Delli <andrea.delli2001@gmail.com>
2025-06-06 15:50:15 +0200

    update evaluation scripts

--- evaluate_openvla.sh
+++ evaluate_openvla.sh
@@ -1,23 +1,29 @@
 #bin/bash
 
-tasks_to_eval="select_fruit select_toy select_poker select_painting select_mahjong" 
+conda activate vlabench
+
 eval_tracks=("track_1_in_distribution" "track_2_cross_category" "track_3_common_sense" "track_4_semantic_instruction" "track_6_unseen_texture")
 
 EPISODE=50
 POLICY="openvla"
-LORA_CKPT="/remote-home1/sdzhang/project/openvla/weights/openvla-7b+vlabench_primitive+b16+lr-0.0005+lora-r32+dropout-0.0"
-
+MODEL_CKPT="checkpoints/openvla-7b"
+# LORA_CKPT=""
+SAVE_DIR="logs"
 METRICS="success_rate intention_score progress_score"
 
 for eval_track in "${eval_tracks[@]}"; do
-    echo "Evaluating on eval track: $eval_track"
+    echo -e "\n \033[0;32m-------------------- Evaluating on eval track: $eval_track ------------------- \033[0m\n"
+    mkdir -p $SAVE_DIR/$eval_track
+    touch $SAVE_DIR/$eval_track/execution_log.txt
+
     python scripts/evaluate_policy.py \
-        --tasks $tasks_to_eval \
         --eval-track $eval_track \
         --n-episode $EPISODE \
         --policy $POLICY \
-        --lora_ckpt $LORA_CKPT\
-        --visulization \
+        --model_ckpt $MODEL_CKPT \
+        --lora_ckpt "" \
+        --visualization \
+        --save-dir $SAVE_DIR \
         --metrics $METRICS \
-
+     2>&1 | tee $SAVE_DIR/$eval_track/execution_log.txt
 done
